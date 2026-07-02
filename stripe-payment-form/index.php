77570044dfb8817c8b4083a1a75e377cf436ac7a
Arshdeep Singh <apsingh.hans@gmail.com>
2026-07-02 10:20:20 +0530

    Started working on plugin

---
+++ stripe-payment-form/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Test Plugin for Testing
+Description: Description for testing purpose.
+Author: Arshdeep Singh
+Version: 1.0
+*/
+
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
