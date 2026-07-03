7b6b4856fcd48990a3cb5d8747f401a748f222cf
Arshdeep Singh <apsingh.hans@gmail.com>
2026-07-03 14:41:56 +0530

    yesterday's code

--- stripe-payment-form/templates/stripe_payment_form.php
+++ stripe-payment-form/templates/stripe_payment_form.php
@@ -3,6 +3,32 @@
 if (!defined('ABSPATH')) {
     exit;
 }
+
+$sk = "";
+
+$url = "";
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPhEADER, [
+    "Content-Type: application/json",
+    "Authorization: Bearer "
+]);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+
+$response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+    echo curl_error($ch);
+} else {
+    echo $response;
+}
+
 ?>
 
-<div></div>
+<div>
+
+</div>
